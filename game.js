9317dc9450a5f8e8c46acccbac65fc01bad8b21b
Pedro Donda <phfdonda@gmail.com>
2020-09-25 18:33:35 -0300

    step 2 - nextSequence function

---
+++ game.js
@@ -0,0 +1,7 @@
+const buttonColors = ["red", "blue", "green", "yellow"]
+let gamePattern = []
+
+function nextSequence(){
+  let randomNumber = Math.floor(Math.random() * 4)
+  let randomChosenColor = buttonColors[randomNumber]
+}
